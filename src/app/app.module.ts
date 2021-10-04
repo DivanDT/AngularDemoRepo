0127f77f8e09fd8af65ce2d415109150a68b98d9
Divan du Toit <divandt@gmail.com>
2021-10-04 16:55:45 +0200

    add basic routing and welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimePeriodPipe } from './pipes/time-period.pipe';
 import { SalaryListComponent } from './salary-list/salary-list.component';
+import { SalaryDetailComponent } from './salary-detail/salary-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SalaryListComponent,
     TimePeriodPipe,
+    SalaryDetailComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'salary', component: SalaryListComponent},
+      {path: 'salary/:id', component: SalaryDetailComponent},
+      {path: 'welcome',component: WelcomeComponent},
+      {path: '',redirectTo: 'welcome',pathMatch: 'full'} ,
+      {path: '**',redirectTo: 'welcome',pathMatch: 'full'} 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
